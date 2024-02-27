84a0698ac43d85e65e60c5f2313282d4678ae91f
lagan <lagan@>
2024-02-27 11:44:26 +0530

    bds_exp5

--- BDS_exp5.py
+++ BDS_exp5.py
@@ -4,21 +4,22 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
 # Generate RSA private and public keys
-private_key = rsa.generate_private_key(
+private_key=rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048
 )
-public_key = private_key.public_key()
+public_key=private_key.public_key()
 
 # Serialize the public key for storage or transmission
-public_key_bytes = public_key.public_bytes(
+public_key_bytes=public_key.public_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PublicFormat.SubjectPublicKeyInfo
 )
 
+
 # Encrypt data using the public key
-def encrypt_data(data, public_key):
-    encrypted_data = public_key.encrypt(
+def encrypt_data(data,public_key):
+    encrypted_data=public_key.encrypt(
         data.encode(),
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -28,9 +29,10 @@ def encrypt_data(data, public_key):
     )
     return encrypted_data
 
+
 # Decrypt data using the private key
-def decrypt_data(encrypted_data, private_key):
-    decrypted_data = private_key.decrypt(
+def decrypt_data(encrypted_data,private_key):
+    decrypted_data=private_key.decrypt(
         encrypted_data,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -40,12 +42,13 @@ def decrypt_data(encrypted_data, private_key):
     )
     return decrypted_data.decode()
 
+
 # Example usage
-data_to_encrypt = "Hello, World!"
-print("Original Data:", data_to_encrypt)
+data_to_encrypt="Lagan Verma, 20BCS3515"
+print("Original Data:",data_to_encrypt)
 
-encrypted_data = encrypt_data(data_to_encrypt, public_key)
-print("Encrypted:", encrypted_data.hex())
+encrypted_data=encrypt_data(data_to_encrypt,public_key)
+print("Encrypted:",encrypted_data.hex())
 
-decrypted_data = decrypt_data(encrypted_data, private_key)
-print("Decrypted:", decrypted_data)
+decrypted_data=decrypt_data(encrypted_data,private_key)
+print("Decrypted:",decrypted_data)
